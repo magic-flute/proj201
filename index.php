13489bd1248d07835af1330446a4af40b84dd40c
magic-flute <mail706843@mailout.e4ward.com>
2016-12-26 02:11:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,9 @@
 echo "Hello world!\n";
 ?>
 Ab cd!.
+<?php
+# This function reads your DATABASE_URL configuration automatically set by Heroku
+# the return value is a string that will work with pg_connect
+$result ="hello world.\n";
+echo $result;
+?>
